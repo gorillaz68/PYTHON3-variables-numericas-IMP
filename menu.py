313be196daa0f2ad354d7555a09c05649856cce9
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-14 11:58:57 -0400

    Create menu.py
    Modulo funcional Limite tipo Interfaz Menu

---
+++ menu.py
@@ -0,0 +1,8 @@
+def menu():
+    print ("1.-Sumar")
+    print ("2.-Restar")
+    print ("3.-multiplicar")
+    print ("4.-Dividir")
+    print ("5.-Salir")
+    op = input("seleccione la opcion:")
+    return op
